64b03dc8881a5426f6eb81297b137f60a573091b
Scott Wiedemann <scott.wiedemann.14@gmail.com>
2025-01-06 15:47:04 -0700

    Add clock and set FPS to 60

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -17,6 +21,7 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
