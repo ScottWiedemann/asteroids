42d850d412b3a07e88f584973ccc9b2a7ee61d68
Scott Wiedemann <scott.wiedemann.14@gmail.com>
2025-01-08 23:21:39 -0700

    Add the ability to shoot to player

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from player import Player
+from shot import Shot
 
 
 def main():
@@ -18,10 +19,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
 
     player = Player(x, y)
     asteroidfield = AsteroidField()
