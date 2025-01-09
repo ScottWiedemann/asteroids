863e83d447847baf1dbca5c481cf6da077256609
Scott Wiedemann <scott.wiedemann.14@gmail.com>
2025-01-09 10:11:24 -0700

    Make astroids split when hit by a shot

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def main():
 
             for shot in shots:
                 if asteroid.collidedwith(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         screen.fill("black")
